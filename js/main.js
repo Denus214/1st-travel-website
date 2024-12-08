b98c5800353509c0e5735a15192664cba4157f53
Denus <brawlstars110pro@gmail.com>
2024-12-08 21:47:39 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -8,3 +8,18 @@
         }
     };
 }());
+
+(function  () {
+
+    const BurgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header__nav');
+    const closeMenuItem = document.querySelector('.header__nav-close');
+    BurgerItem.addEventListener('click', () => {
+        menu.classList.add('header__nav-active');
+    });
+
+    closeMenuItem.addEventListener('click', () => {
+        menu.classList.remove('header__nav-active');
+    });
+
+}());
